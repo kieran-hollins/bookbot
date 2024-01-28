e5412d6c1cda9cfeae09a878a3b6026f47601b32
= <khollins123@gmail.com>
2024-01-28 22:37:30 +0000

    updated to break up program into separate functions

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-with open('books/frankenstein.txt') as f:
-    file_contents = f.read()
 
 def main():
-    print(file_contents)
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+
+    num_words = get_num_words(text)
+    print(f"Frankenstein contains {num_words} words")
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
 
 main()
